73c1f5efd0b3775a671e9522450466439fcc49fb
Fernando López <fernando.lopez.penalver@gmail.com>
2015-08-03 22:23:50 +0200

    Recrear base de datos

--- models/models.js
+++ models/models.js
@@ -33,8 +33,9 @@ var Quiz = sequelize.import(quiz_path);
 exports.Quiz = Quiz; // exportar tabla Quiz
 
 // sequelize.sync() inicializa tabla de preguntas en DB
-sequelize.sync().then(function() {
+sequelize.sync({force: true}).then(function() {
   // then(..) ejecuta el manejador una vez creada la tabla
+  
   Quiz.count().then(function (count){
     if(count === 0) {   // la tabla se inicializa solo si está vacía
       Quiz.bulkCreate( 
