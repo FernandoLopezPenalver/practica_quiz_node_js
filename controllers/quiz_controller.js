9bed0e6a728f0725a9b188f3bea5c8514da264d9
Fernando López <fernando.lopez.penalver@gmail.com>
2015-08-03 21:18:01 +0200

    Borrar pregunta

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -108,3 +108,12 @@ exports.update = function(req, res) {
     }
   );
 };
+
+// DELETE /quizes/:id
+exports.destroy = function(req, res) {
+  console.log("quiz_controller.destroy");
+  req.quiz.destroy().then( function() {
+    res.redirect('/quizes');
+  }).catch(function(error){next(error)});
+};
+
