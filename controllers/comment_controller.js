6bba92f478a11794999dd8d84794b8df5a438e45
Fernando López <fernando.lopez.penalver@gmail.com>
2015-08-05 14:08:47 +0200

    Moderación de comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,7 +1,24 @@
 var models = require('../models/models.js');
 
+// Autoload :id de comentarios
+exports.load = function(req, res, next, commentId) {
+  console.log("comment_controller.load");
+  models.Comment.find({
+            where: {
+                id: Number(commentId)
+            }
+        }).then(function(comment) {
+      if (comment) {
+        req.comment = comment;
+        next();
+      } else{next(new Error('No existe commentId=' + commentId))}
+    }
+  ).catch(function(error){next(error)});
+};
+
 // GET /quizes/:quizId/comments/new
 exports.new = function(req, res) {
+  console.log("comment_controller.new");
   res.render('comments/new.ejs', {quizid: req.params.quizId, errors: []});
 };
 
@@ -27,4 +44,15 @@ exports.create = function(req, res) {
     }
   ).catch(function(error){next(error)});
   
+};
+
+// GET /quizes/:quizId/comments/:commentId/publish
+exports.publish = function(req, res) {
+  console.log("comment_controller.publish");
+  req.comment.publicado = true;
+
+  req.comment.save( {fields: ["publicado"]})
+    .then( function(){ res.redirect('/quizes/'+req.params.quizId);} )
+    .catch(function(error){next(error)});
+
 };
